ae032aa354a6981e8bf5cdf67f0eec066fb1ee0f
Gareth <gareth.lambe@plannet21.ie>
2020-01-23 22:49:33 +0000

    Fixed Db import when running

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from db import db
+
 from resources.store import Store, StoreList
 
 
@@ -26,5 +26,6 @@ api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug = True)
